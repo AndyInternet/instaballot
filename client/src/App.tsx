c4bea00e9f4e15a3a1f4cd8c651b731302a27243
Andy Lawrence <55452023+AndyInternet@users.noreply.github.com>
2023-01-03 01:53:50 -0500

    update vite config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { Container } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { DataLoader } from './helpers/DataLoader';
 import { IsMobile } from './helpers/IsMobile';
 import { ThemeManager } from './helpers/ThemeManager';
@@ -9,12 +11,8 @@ import { Ballots } from './views/Ballots';
 import { Help } from './views/Help';
 import { NewQuestion } from './views/NewQuestion';
 import { NoMatch } from './views/NoMatch';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { getFingerprint } from './utils/getFingerprint';
 
 export const App = () => {
-  getFingerprint();
   return (
     <Router>
       <DataLoader />
@@ -23,7 +21,7 @@ export const App = () => {
         <Container sx={{ paddingTop: '80px', paddingBottom: '80px' }}>
           <TopBar />
           <Routes>
-            <Route path='/' element={<Ballots />} />
+            <Route index path='/' element={<Ballots />} />
             <Route path='new' element={<NewQuestion />} />
             <Route path='help' element={<Help />} />
             <Route path='*' element={<NoMatch />} />
