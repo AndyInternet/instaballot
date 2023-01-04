fd55289897f5d016ce9509b39f5fa24073612157
Andy Lawrence <55452023+AndyInternet@users.noreply.github.com>
2023-01-03 22:59:09 -0500

    Fix NaN issue with vote % when no votes cast

--- client/src/utils/getVotePercentage.ts
+++ client/src/utils/getVotePercentage.ts
@@ -5,8 +5,12 @@ export const getVotePercentage = (
   answerId: string | undefined,
 ) => {
   const totalVotes = votes.length;
+
+  if (totalVotes === 0) return 0;
+
   const votesForAnswer = votes.filter(
     (vote) => vote.answerId === answerId,
   ).length;
+
   return (votesForAnswer / totalVotes) * 100;
 };
