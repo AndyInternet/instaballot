dc0c8afdc07ca23e207289a88221172898062eb3
Andy Lawrence <55452023+AndyInternet@users.noreply.github.com>
2023-01-04 00:24:20 -0500

    5

--- server/src/middleware/fingerprint.ts
+++ server/src/middleware/fingerprint.ts
@@ -4,10 +4,9 @@ import { v4 } from 'uuid';
 import { CustomRequest as Request, CustomResponse as Response } from '../types';
 
 export const fingerprint = async (req: Request, res: Response, next: NextFunction) => {
-  const domain = process.env.CLIENT_HOSTNAME;
   let fingerprint = req.cookies?.fingerprint;
   if (!fingerprint) fingerprint = v4();
-  res.cookie('fingerprint', fingerprint, { domain: domain, secure: false, expires: dayjs().add(1, 'year').toDate() });
+  res.cookie('fingerprint', fingerprint, { secure: false, expires: dayjs().add(1, 'year').toDate() });
   req.fingerprint = fingerprint;
   next();
 };
