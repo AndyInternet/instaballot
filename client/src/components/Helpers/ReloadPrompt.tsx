6e3c66771a648837156ff5f71d4274d670437f61
Andy Lawrence <55452023+AndyInternet@users.noreply.github.com>
2023-01-04 10:19:42 -0500

    Reload pwa implemented

---
+++ client/src/components/Helpers/ReloadPrompt.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Typography,
+} from '@mui/material';
+import { useRegisterSW } from 'virtual:pwa-register/react';
+
+export const ReloadPrompt = () => {
+  const {
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered(r) {
+      // eslint-disable-next-line prefer-template
+      console.info('SW Registered: ' + r);
+    },
+    onRegisterError(error) {
+      console.error('SW registration error', error);
+    },
+  });
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(needRefresh);
+  }, [needRefresh]);
+
+  const handleClose = () => {
+    setNeedRefresh(false);
+    setOpen(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby='reload-prompt'
+      aria-describedby='reload-description'
+    >
+      <DialogContent>
+        <DialogContentText id='reload-description'>
+          <Typography>
+            New content available, click on reload button to update.
+          </Typography>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button autoFocus onClick={() => updateServiceWorker(true)}>
+          Reload
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
