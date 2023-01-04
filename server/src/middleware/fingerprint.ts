7e33148b538e3085252f7cb3348d4b369bc5597e
Andy Lawrence <55452023+AndyInternet@users.noreply.github.com>
2023-01-04 00:10:15 -0500

    secure cookie update

--- server/src/middleware/fingerprint.ts
+++ server/src/middleware/fingerprint.ts
@@ -4,9 +4,10 @@ import { v4 } from 'uuid';
 import { CustomRequest as Request, CustomResponse as Response } from '../types';
 
 export const fingerprint = async (req: Request, res: Response, next: NextFunction) => {
+  const isSecure = process.env.CLIENT_HOSTNAME?.includes('https');
   let fingerprint = req.cookies?.fingerprint;
   if (!fingerprint) fingerprint = v4();
-  res.cookie('fingerprint', fingerprint, { httpOnly: false, expires: dayjs().add(1, 'year').toDate() });
+  res.cookie('fingerprint', fingerprint, { secure: isSecure, expires: dayjs().add(1, 'year').toDate() });
   req.fingerprint = fingerprint;
   next();
 };
